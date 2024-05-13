de3908984bdd5de19f09c5dab05a520b9d00a3c3
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-05-13 23:16:28 +0300

    UPD: Hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,34 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function Home() {
-  return <div>This is my portfolio app</div>
+export default function Homepage() {
+    return (
+        <div className="h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+            {/* IMAGE CONTAINER */}
+            <div className="h-1/2 relative">
+                <Image src="/hero.png" alt="" fill className="object-contain" />
+            </div>
+            {/* TEXT CONTAINER */}
+            <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+                <h1 className="text-4xl font-bold">
+                    Crafting Digital Experiences, Designing Tomorrow.
+                </h1>
+                {/* DESCRIPTION */}
+                <p className="">
+                    Welcome to my digital canvas, where innovation and
+                    creativity converge. With a keen eye for aesthetics and a
+                    mastery of code, my portfolio showcases a diverse collection
+                    of projects that reflect my commitment to excellence.
+                </p>
+                {/* BUTTONS */}
+                <div className=" flex gap-4">
+                    <button className="p-4 rounded-lg ring ring-black bg-black text-white">
+                        View My Work
+                    </button>
+                    <button className="p-4 rounded-lg ring ring-black">
+                        Contact Me
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -17,4 +17,3 @@ const NavLink = ({ link }) => {
 };
 
 export default NavLink;
-scrollTo;
