d6e839a028f3d680f2642d014ecd6133dc007cf3
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-05-13 08:31:17 +0300

    test

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,28 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+
+const links = [
+    {
+        url: '/',
+        title: 'Home',
+    },
+    {
+        url: '/about',
+        title: 'About',
+    },
+    {
+        url: '/portfolio',
+        title: 'Portfolio',
+    },
+    {
+        url: '/contact',
+        title: 'Contact',
+    },
+];
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
             {/*LOGO  */}
@@ -19,11 +40,26 @@ const Navbar = () => {
             {/* RESPONSIVE MENU */}
             <div className="">
                 {/* MENU BUTTON */}
-                <button className="w-10 h-8 flex flex-col justify-between">
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
+                <button
+                    className="w-10 h-8 flex flex-col justify-between z-50 relative"
+                    onClick={() => {
+                        setOpen(!open);
+                    }}
+                >
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
                 </button>
+                {/* MENU LIST */}
+                {open && (
+                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl">
+                        {links.map(link => (
+                            <Link key={link.title} href={link.url}>
+                                {link.title}
+                            </Link>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
