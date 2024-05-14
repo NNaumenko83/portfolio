78906566b37c7190c9b33ae950e97d7a53f366a1
Mykola <nnv2006@ukr.net>
2024-05-14 16:30:21 +0300

    ADD: framer-motion

--- src/app/page.js
+++ src/app/page.js
@@ -2,26 +2,26 @@ import Image from 'next/image';
 
 export default function Homepage() {
     return (
-        <div className="h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+        <div className="flex flex-col h-full px-4 lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
             {/* IMAGE CONTAINER */}
-            <div className="h-1/2 relative">
+            <div className="relative lg:h-full lg:w-1/2 h-1/2">
                 <Image src="/hero.png" alt="" fill className="object-contain" />
             </div>
             {/* TEXT CONTAINER */}
-            <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
-                <h1 className="text-4xl font-bold">
+            <div className="flex flex-col items-center justify-center gap-8 h-1/2 lg:h-full lg:w-1/2">
+                <h1 className="text-4xl font-bold md:text-6xl">
                     Crafting Digital Experiences, Designing Tomorrow.
                 </h1>
                 {/* DESCRIPTION */}
-                <p className="">
+                <p className="md:text-xl">
                     Welcome to my digital canvas, where innovation and
                     creativity converge. With a keen eye for aesthetics and a
                     mastery of code, my portfolio showcases a diverse collection
                     of projects that reflect my commitment to excellence.
                 </p>
                 {/* BUTTONS */}
-                <div className=" flex gap-4">
-                    <button className="p-4 rounded-lg ring ring-black bg-black text-white">
+                <div className="flex w-full gap-4">
+                    <button className="p-4 text-white bg-black rounded-lg ring ring-black">
                         View My Work
                     </button>
                     <button className="p-4 rounded-lg ring ring-black">
