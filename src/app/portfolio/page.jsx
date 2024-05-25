382afe888b10722731ae429c072e63c94dc404c9
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-05-25 16:27:38 +0300

    UPD: portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -67,12 +67,23 @@ const PortfolioPage = () => {
                                 key={item.id}
                             >
                                 <div className="flex flex-col gap-8 text-w">
-                                    <h1>{item.title}</h1>
-                                    <div className="relative">
+                                    <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl ">
+                                        {item.title}
+                                    </h1>
+                                    <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[50px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                                         <Image src={item.img} alt="" fill />
                                     </div>
-                                    <p>{item.desc}</p>
-                                    <Link href={item.link}>See Demo</Link>
+                                    <p className="w-80 md:w96 lg:w-[500px] ld:text-lg xl:w-[600px]">
+                                        {item.desc}
+                                    </p>
+                                    <Link
+                                        href={item.link}
+                                        className="flex justify-end"
+                                    >
+                                        <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                                            See Demo
+                                        </button>
+                                    </Link>
                                 </div>
                             </div>
                         ))}
@@ -102,7 +113,6 @@ const PortfolioPage = () => {
                             <textPath
                                 xlinkHref="#circlePath"
                                 className="text-3xl"
-                                startOffset="0%"
                             >
                                 Full-Stack Developer
                             </textPath>
